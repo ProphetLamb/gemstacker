794eba828f462ad82ef795460e2203b88fe42c1d
Prophet Lamb <prophet.lamb@gmail.com>
2023-09-12 21:30:27 +0200

    Fix rename main.py

--- lib/serve.sh
+++ lib/serve.sh
@@ -2,4 +2,4 @@
 # -*- coding: utf-8 -*-
 python3 -m venv .venv
 .venv/bin/pip3 install -r requirements.txt
-.venv/bin/python3 -m uvicorn --factory get_listings:app --host 127.0.0.1 --port 8000 --reload
+.venv/bin/python3 -m uvicorn --factory main:app --host 127.0.0.1 --port 8000 --reload
