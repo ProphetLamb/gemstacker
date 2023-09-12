752e6df489fc8abebe3cf6b20daa8f479a618c34
Prophet Lamb <prophet.lamb@gmail.com>
2023-09-12 21:36:48 +0200

    Add test request

--- lib/serve.sh
+++ lib/serve.sh
@@ -2,4 +2,10 @@
 # -*- coding: utf-8 -*-
 python3 -m venv .venv
 .venv/bin/pip3 install -r requirements.txt
+
+export DEBUG="true"
+export WORKERS="4"
+export LOG_LEVEL="debug"
+export ALLOWED_ORIGINS="*"
+export BEARER_TOKEN="test"
 .venv/bin/python3 -m uvicorn --factory main:app --host 127.0.0.1 --port 8000 --reload
