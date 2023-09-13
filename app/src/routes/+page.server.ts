6201971ac9ff071a56e05a155b96f315742e88ec
ProphetLamb <prophet.lamb@gmail.com>
2023-09-13 13:01:40 +0200

    Render results property

--- app/src/routes/+page.server.ts
+++ app/src/routes/+page.server.ts
@@ -1,36 +1,43 @@
-import { fail } from '@sveltejs/kit';
+import type { Actions } from '@sveltejs/kit';
 import { API_KEY, API_ENDPOINT } from '$env/static/private';
 import { GemProfitApi } from '$lib/server/gemLevelProfitApi';
 import { z } from 'zod';
-import { superValidate } from 'sveltekit-superforms/dist/superValidate';
 import type { PageServerLoad } from './$types';
+import { superValidate } from 'sveltekit-superforms/client';
 
 const schema = z.object({
-	gem_name: z.string(),
+	gem_name: z.string().optional(),
 	min_sell_price_chaos: z.number().optional(),
 	max_buy_price_chaos: z.number().optional(),
-	min_experience_delta: z.number().min(1000000).max(100000000),
+	min_experience_delta: z.number().min(1000000).max(100000000).optional().default(10000000),
 	items_offset: z.number().optional().default(0),
 	items_count: z.number().optional().default(10)
 });
 
-export const load: PageServerLoad = async ({ request, fetch }) => {
-	const form = await superValidate(request, schema);
+export const load: PageServerLoad = async () => {
+	const form = await superValidate(schema);
+	return { form }
+};
 
-	if (!form.valid) {
-		return fail(400, { form });
-	}
+export const actions: Actions = {
+	default: async ({ fetch, request }) => {
+		const form = await superValidate(request, schema);
 
-	const gemProfitApi = new GemProfitApi(fetch, {
-		api_endpoint: API_ENDPOINT,
-		api_key: API_KEY
-	});
+		if (!form.valid) {
+			return { form };
+		}
 
-	try {
-		const gemProfit = await gemProfitApi.getGemProfit(form.data);
+		const gemProfitApi = new GemProfitApi(fetch, {
+			api_endpoint: API_ENDPOINT,
+			api_key: API_KEY
+		});
 
-		return { form, gemProfit: Promise.resolve(gemProfit) };
-	} catch (error) {
-		return fail(500, { form, error });
+		try {
+			const gemProfit = await gemProfitApi.getGemProfit(form.data);
+
+			return { form, gemProfit };
+		} catch (error) {
+			return { form };
+		}
 	}
 };
