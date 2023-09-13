ba46965b8669c4cb77639182734975d4dcc23dae
ProphetLamb <prophet.lamb@gmail.com>
2023-09-13 09:52:02 +0200

    move variables to .env.example

--- lib/test-serve.sh
+++ lib/test-serve.sh
@@ -3,9 +3,7 @@
 python3 -m venv .venv
 .venv/bin/pip3 install -r requirements.txt
 
-export DEBUG="true"
-export WORKERS="4"
-export LOG_LEVEL="debug"
-export ALLOWED_ORIGINS="*"
-export API_KEY="test"
+# load .env file
+export $(grep -v '^#' .env.example | xargs)
+# start server
 .venv/bin/python3 -m uvicorn --factory main:app --host 127.0.0.1 --port 8000 --reload
