def000f8159dcd107308a8eb48861182e5d4e7a6
Prophet Lamb <prophet.lamb@gmail.com>
2023-09-14 22:18:55 +0200

    Move interfaces to shared

--- app/src/lib/gemLevelProfitApi.ts
+++ app/src/lib/gemLevelProfitApi.ts
@@ -8,3 +8,62 @@ export const gemLevelsProfitSchema = z.object({
 	items_offset: z.number().optional().default(0),
 	items_count: z.number().optional().default(10)
 });
+
+export interface GemProfitRequestParameter {
+	gem_name?: string;
+	min_sell_price_chaos?: number;
+	max_buy_price_chaos?: number;
+	min_experience_delta?: number;
+	items_offset?: number;
+	items_count?: number;
+}
+
+export const GemProfitRequestParameterSchema = z.object({
+	gem_name: z.string().optional(),
+	min_sell_price_chaos: z.number().optional(),
+	max_buy_price_chaos: z.number().optional(),
+	min_experience_delta: z.number().optional(),
+	items_offset: z.number().optional(),
+	items_count: z.number().optional()
+});
+
+export interface GemProfitResponse {
+	data: {
+		[key: string]: {
+			min: {
+				price: number;
+				level: number;
+				exp: number;
+			};
+			max: {
+				price: number;
+				level: number;
+				exp: number;
+			};
+			gain_margin: number;
+		};
+	};
+}
+
+export const GemProfitResponseSchema = z.object({
+	data: z.record(
+		z.object({
+			min: z.object({
+				price: z.number(),
+				level: z.number(),
+				exp: z.number()
+			}),
+			max: z.object({
+				price: z.number(),
+				level: z.number(),
+				exp: z.number()
+			}),
+			gain_margin: z.number()
+		})
+	)
+});
+
+export interface GemProfitApiOptions {
+	api_endpoint: string;
+	api_key: string;
+}
--- app/src/lib/server/gemLevelProfitApi.ts
+++ app/src/lib/server/gemLevelProfitApi.ts
@@ -1,74 +1,39 @@
-import { removeNull } from "$lib/typing";
-
-export interface GemProfitRequestParameter {
-	gem_name?: string;
-	min_sell_price_chaos?: number;
-	max_buy_price_chaos?: number;
-	min_experience_delta?: number;
-	items_offset?: number;
-	items_count?: number;
-}
-
-export interface GemProfitResponse {
-	data: {
-		[key: string]: {
-			min: {
-				price: number;
-				level: number;
-				exp: number;
-			};
-			max: {
-				price: number;
-				level: number;
-				exp: number;
-			};
-			gain_margin: number;
-		};
-	};
-}
-
-export interface GemProfitApiOptions {
-	api_endpoint: string;
-	api_key: string;
-}
+import {
+	GemProfitRequestParameterSchema,
+	type GemProfitApiOptions,
+	type GemProfitRequestParameter,
+	type GemProfitResponse,
+	GemProfitResponseSchema
+} from '$lib/gemLevelProfitApi';
 
 type Fetch = (input: RequestInfo | URL, init?: RequestInit | undefined) => Promise<Response>;
 
 export class GemProfitApi {
 	fetch: Fetch;
 	options: GemProfitApiOptions;
-	constructor(
-		fetch: Fetch,
-		options: GemProfitApiOptions
-	) {
+	constructor(fetch: Fetch, options: GemProfitApiOptions) {
 		this.options = options;
 		this.fetch = fetch;
 	}
-
 	getGemProfit: (param: GemProfitRequestParameter) => Promise<GemProfitResponse> = async (
 		param
 	) => {
-		const paramIdentity = removeNull({
-			gem_name: param.gem_name?.toString(),
-			min_sell_price_chaos: param.min_sell_price_chaos?.toString(),
-			max_buy_price_chaos: param.max_buy_price_chaos?.toString(),
-			min_experience_delta: param.min_experience_delta?.toString(),
-			items_offset: param.items_offset?.toString(),
-			items_count: param.items_count?.toString()
-		});
+		GemProfitRequestParameterSchema.parse(param);
 
-		const queryParam = new URLSearchParams(paramIdentity);
+		const queryParam = Object.entries(param)
+			.map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value)}`)
+			.join('&');
 		const headers = new Headers({
 			Accept: 'application/json',
 			Authorization: `Bearer ${btoa(this.options.api_key)}`
 		});
-		const url = new URL(`${this.options.api_endpoint}/gem-profit`);
-		url.search = queryParam.toString();
+		const url = new URL(`${this.options.api_endpoint}/gem-profit?${queryParam}`);
 		const response = await this.fetch(url.toString(), { headers });
 		if (response.status !== 200) {
 			throw new Error(`Request failed with status ${response.status}: ${await response.text()}`);
 		}
-		const result = (await response.json()) as GemProfitResponse;
+		const rawResult = await response.json();
+		const result = GemProfitResponseSchema.parse(rawResult);
 		return result;
 	};
 }
