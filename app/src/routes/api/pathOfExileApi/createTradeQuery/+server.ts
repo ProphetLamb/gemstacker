afec785bec09b395ffa7e2b10c56304f35bc9016
Prophet Lamb <prophet.lamb@gmail.com>
2023-09-14 23:02:35 +0200

    Implement api createTradeQuery

--- app/src/routes/api/pathOfExileApi/createTradeQuery/+server.ts
+++ app/src/routes/api/pathOfExileApi/createTradeQuery/+server.ts
@@ -1,10 +1,22 @@
+import { poeTradeGemRequestSchema, type PoeTradeLeagueRequest } from '$lib/pathOfExileApi';
 import { PathofExileApi } from '$lib/server/pathOfExileApi';
-import type { RequestHandler } from '@sveltejs/kit';
+import { error, json, type RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async ({ fetch, request }) => {
-	const rawParam = request.json();
-	const param = GemProfitRequestParameterSchema.;
-
+	const requestBody = request.json();
+	const tradeQuery = poeTradeGemRequestSchema.safeParse(requestBody);
+	if (!tradeQuery.success) {
+		throw error(400, { message: tradeQuery.error.message });
+	}
+	const league = { realm: 'pc', trade_league: 'Softcore' } satisfies PoeTradeLeagueRequest;
 	const api = new PathofExileApi(fetch, {});
-	await api.createTradeQuery();
+	try {
+		const result = await api.createTradeQuery({ type: 'gem', ...league, ...tradeQuery.data });
+		return json({ url: result.web_trade_url });
+	} catch (e) {
+		if (e instanceof Error) {
+			throw error(500, { message: e.message });
+		}
+		throw error(500, { message: 'Something went wrong' });
+	}
 };
