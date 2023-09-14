3fbfc1abc476fba896da99dfc795f46769975768
ProphetLamb <prophet.lamb@gmail.com>
2023-09-14 14:48:34 +0200

    Fix typo

--- app/src/lib/gemLevelProfitApi.ts
+++ app/src/lib/gemLevelProfitApi.ts
@@ -1,4 +1,14 @@
 import { removeNull } from "$lib/typing";
+import { z } from 'zod';
+
+export const gemLevelsProfitSchema = z.object({
+	gem_name: z.string().optional(),
+	min_sell_price_chaos: z.number().optional(),
+	max_buy_price_chaos: z.number().optional(),
+	min_experience_delta: z.number().min(1000000).max(100000000).default(1000000),
+	items_offset: z.number().optional().default(0),
+	items_count: z.number().optional().default(10)
+});
 
 export interface GemProfitRequestParameter {
 	gem_name?: string;
