32b6e181456a63b81d6d398024fb9fd8ef701fd3
Prophet Lamb <prophet.lamb@gmail.com>
2023-09-14 22:19:01 +0200

    drop typing

--- app/src/lib/typing.ts
+++
@@ -1,9 +0,0 @@
-export function removeNull<K extends string | number | symbol, T>(obj: Record<K, T | null | undefined>): Record<K, T> {
-	const result: Record<K, T> = {} as Record<K, T>;
-	for (const key in obj) {
-		if (obj[key] !== null && obj[key] !== undefined) {
-			result[key] = obj[key] as T;
-		}
-	}
-	return result;
-}
