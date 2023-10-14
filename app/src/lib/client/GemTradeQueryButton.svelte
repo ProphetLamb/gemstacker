e71697f52f6d286ea3c2bf3acb9a90ff57f160f5
Prophet Lamb <prophet.lamb@gmail.com>
2023-10-14 19:11:14 +0200

    Update trade url generator

--- app/src/lib/client/GemTradeQueryButton.svelte
+++ app/src/lib/client/GemTradeQueryButton.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { GemProfitResponseItem } from '$lib/gemLevelProfitApi';
-	import { poeTradeQueryResponseSchema, type PoeTradeGemRequest } from '$lib/pathOfExileApi';
+	import { createGemTradeQueryBody, type PoeTradeGemRequest } from '$lib/pathOfExileApi';
 	import { ProgressRadial } from '@skeletonlabs/skeleton';
 
 	export let data: GemProfitResponseItem;
@@ -8,27 +8,21 @@
 
 	let web_trade_url: Promise<string | undefined> = Promise.resolve(undefined);
 
-	function startCreateTradeQuery() {
-		web_trade_url = fetch('/api/pathOfExile/createTradeQuery', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
+	function getTradeQueryUrl() {
+		let body = JSON.stringify(
+			createGemTradeQueryBody({
 				name,
-				min_level: data.min.level,
+				corrupted: false,
 				max_level: data.max.level,
-				corrupted: false
-			} satisfies PoeTradeGemRequest)
-		}).then(async (res) => {
-			if (res.status !== 200) {
-				return undefined;
-			}
-			const rawResponse = await res.json();
-			console.log(rawResponse);
-			const response = poeTradeQueryResponseSchema.parse(rawResponse);
-			return response.web_trade_url;
-		});
+				min_level: data.min.level
+			})
+		);
+		let url = `https://www.pathofexile.com/trade/search/Ancestor?q=${body}`;
+		return url;
+	}
+
+	function startSetTradeDecoupledQuery() {
+		web_trade_url = Promise.resolve(getTradeQueryUrl());
 	}
 </script>
 
@@ -40,7 +34,8 @@
 	{#if web_trade_url}
 		<a class="btn variant-soft-success w-[5rem]" href={web_trade_url} target="_blank">Buy</a>
 	{:else}
-		<button class="btn variant-soft-primary w-[5rem]" on:click={startCreateTradeQuery}>Trade</button
+		<button class="btn variant-soft-primary w-[5rem]" on:click={startSetTradeDecoupledQuery}
+			>Trade</button
 		>
 	{/if}
 {:catch error}
