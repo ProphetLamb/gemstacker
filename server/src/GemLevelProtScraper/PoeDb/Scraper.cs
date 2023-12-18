16f3c46a5b56623e7aa4ea0784fff749b950252a
ProphetLamb <prophet.lamb@gmail.com>
2023-12-18 15:06:46 +0100

    Prefer Attribute table over first table

--- server/src/GemLevelProtScraper/PoeDb/Scraper.cs
+++ server/src/GemLevelProtScraper/PoeDb/Scraper.cs
@@ -84,7 +84,9 @@ internal sealed partial class PoeDbSkillSpider(IDataflowPublisher<PoeDbSkill> sk
                 && TryGetTableForHeader(headers, skillName, skillName) is { } descriptionTable
                 ? new PoeDbSkillDescription(header.ParentElement!.Text(), ParseRelatedGemsTable(descriptionTable).ToImmutableArray())
                 : null;
-            var skillStats = pane.QuerySelector(":scope > table.table") is IHtmlTableElement infoTable
+            var skillStats = TryGetTableForHeader(headers, skillName, "Attribute", logger) is { } attributeTable
+                ? ParseInfoTable(attributeTable)
+                : pane.QuerySelector(":scope > table.table") is IHtmlTableElement infoTable
                 ? ParseInfoTable(infoTable)
                 : throw new InvalidOperationException("Missing skill stats table");
 
