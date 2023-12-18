adfed9a02b71956242a5c97b7f625d11a2849d7a
ProphetLamb <prophet.lamb@gmail.com>
2023-12-18 16:54:23 +0100

    Embed trade URL directly

--- app/src/lib/client/GemTradeQueryButton.svelte
+++ app/src/lib/client/GemTradeQueryButton.svelte
@@ -6,7 +6,7 @@
 	export let data: GemProfitResponseItem;
 	export let name: string;
 
-	let web_trade_url: Promise<string | undefined> = Promise.resolve(undefined);
+	let web_trade_url = getTradeQueryUrl();
 
 	function getTradeQueryUrl() {
 		let body = JSON.stringify(
@@ -20,24 +20,6 @@
 		let url = `https://www.pathofexile.com/trade/search/Ancestor?q=${body}`;
 		return url;
 	}
-
-	function startSetTradeDecoupledQuery() {
-		web_trade_url = Promise.resolve(getTradeQueryUrl());
-	}
 </script>
 
-{#await web_trade_url}
-	<button class="btn variant-soft-primary w-[5rem]" disabled>
-		<ProgressRadial width="w-6" font={10} />
-	</button>
-{:then web_trade_url}
-	{#if web_trade_url}
-		<a class="btn variant-soft-success w-[5rem]" href={web_trade_url} target="_blank">Buy</a>
-	{:else}
-		<button class="btn variant-soft-primary w-[5rem]" on:click={startSetTradeDecoupledQuery}
-			>Trade</button
-		>
-	{/if}
-{:catch error}
-	<button class="btn variant-soft-error w-[5rem]">Failed</button>
-{/await}
+<a class="btn variant-soft-success w-[5rem]" href={web_trade_url} target="_blank">Buy</a>
