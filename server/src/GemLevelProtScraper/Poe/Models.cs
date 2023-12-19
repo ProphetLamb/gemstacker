e8750a63c7c8bd0c3c05bd035626f332a4c50268
ProphetLamb <prophet.lamb@gmail.com>
2023-12-19 22:20:02 +0100

    LeagueMode HarcoreRuthless combination instead of flag

--- server/src/GemLevelProtScraper/Poe/Models.cs
+++ server/src/GemLevelProtScraper/Poe/Models.cs
@@ -7,5 +7,5 @@ public sealed record PoeLeaugeList(string ApiUrl);
 public sealed record PoeLeauge(string Name, string Text, Realm Realm, LeaugeMode Mode);
 
 [Flags]
-public enum LeaugeMode { None, Standard, Softcore, Hardcore, Ruthless, HardcoreRuthless }
+public enum LeaugeMode { None, Standard, Softcore, Hardcore, Ruthless, HardcoreRuthless = Hardcore | Ruthless }
 public enum Realm { Pc, Xbox, Sony }
