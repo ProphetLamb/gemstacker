e435d5e265f1e728f805136adbb55ccba657051c
ProphetLamb <prophet.lamb@gmail.com>
2023-12-20 13:43:58 +0100

    Implement IAsyncEnumerable for DataflowSignal

--- server/src/GemLevelProtScraper/Signal.cs
+++ server/src/GemLevelProtScraper/Signal.cs
@@ -1,14 +1,16 @@
+using System.Runtime.CompilerServices;
 using ScrapeAAS;
 
 namespace GemLevelProtScraper;
 
-public sealed class DataflowSignal<T> : IDataflowHandler<T>
+public sealed class DataflowSignal<T> : IDataflowHandler<T>, IAsyncEnumerable<T>
 {
-    private readonly TaskCompletionSource<T> _tcs = new();
+    private TaskCompletionSource<T> _tcs = new();
 
     ValueTask IDataflowHandler<T>.HandleAsync(T message, CancellationToken cancellationToken)
     {
-        _tcs.TrySetResult(message);
+        var tcs = Interlocked.Exchange(ref _tcs, new());
+        _ = tcs.TrySetResult(message);
         return default;
     }
 
@@ -16,4 +18,37 @@ public sealed class DataflowSignal<T> : IDataflowHandler<T>
     {
         return _tcs.Task.WaitAsync(cancellationToken);
     }
+
+    public async Task<T?> WaitAsync(Func<T, bool> predicate, CancellationToken cancellationToken = default)
+    {
+        await using var en = GetAsyncEnumerator(cancellationToken);
+        while (await en.MoveNextAsync().ConfigureAwait(false))
+        {
+            var item = en.Current;
+            if (predicate(item))
+            {
+                return item;
+            }
+        }
+
+        return default;
+    }
+
+    public IAsyncEnumerator<T> GetAsyncEnumerator(CancellationToken cancellationToken = default)
+    {
+        return Enumerable(this, cancellationToken).GetAsyncEnumerator(cancellationToken);
+
+        static async IAsyncEnumerable<T> Enumerable(DataflowSignal<T> signal, [EnumeratorCancellation] CancellationToken cancellationToken)
+        {
+            var task = signal.WaitAsync(cancellationToken);
+            while (true)
+            {
+                var innerTask = Interlocked.Exchange(ref task, signal.WaitAsync(cancellationToken));
+                var item = await innerTask.ConfigureAwait(false);
+                yield return item;
+                cancellationToken.ThrowIfCancellationRequested();
+            }
+        }
+    }
+
 }
