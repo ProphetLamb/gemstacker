d651f5b357d0974b793f22103a974f7e32e2b64e
ProphetLamb <prophet.lamb@gmail.com>
2023-12-21 15:42:01 +0100

    Add migrations

--- server/src/GemLevelProtScraper/PoeNinja/Migrations.cs
+++ server/src/GemLevelProtScraper/PoeNinja/Migrations.cs
@@ -11,6 +11,8 @@ public sealed record PoeNinjaDatabaseSettings : IOptions<PoeNinjaDatabaseSetting
     public required string DatabaseName { get; init; }
     public required string GemPriceCollectionName { get; init; }
 
+    public const string Alias = "PoeNinja";
+
     PoeNinjaDatabaseSettings IOptions<PoeNinjaDatabaseSettings>.Value => this;
 
     public MongoMigrableDefinition GetMigratableDefinition()
@@ -18,43 +20,95 @@ public sealed record PoeNinjaDatabaseSettings : IOptions<PoeNinjaDatabaseSetting
         return new()
         {
             ConnectionString = ConnectionString,
-            Database = new("PoeNinja", DatabaseName),
+            Database = new(Alias, DatabaseName),
             MirgrationStateCollectionName = "DATABASE_MIGRATIONS"
         };
     }
+
+    internal IMongoCollection<PoeNinjaApiGemPriceEnvalope> GetGemPriceCollection(IMongoDatabase database)
+    {
+        return database.GetCollection<PoeNinjaApiGemPriceEnvalope>(GemPriceCollectionName);
+    }
 }
 
-[MongoMigration("PoeNinja", 0, 1, Description = $"Add composite index {GemIdentifierIndexName}")]
-public sealed class PoeNinjaAddCompositeIndexMigration(IOptions<PoeNinjaDatabaseSettings> optionsAccessor) : IMongoMigration
+[MongoMigration(PoeNinjaDatabaseSettings.Alias, 2, 3, Description = $"Add text index {GemLeagueIndexName}.")]
+public sealed class PoeNinjaAddLeagueIndexMigration(IOptions<PoeNinjaDatabaseSettings> optionsAccessor) : IMongoMigration
+{
+    public const string GemLeagueIndexName = "GemLeague";
+
+    public async Task DownAsync(IMongoDatabase database, CancellationToken cancellationToken = default)
+    {
+        var col = optionsAccessor.Value.GetGemPriceCollection(database);
+        await col.Indexes.DropOneAsync(GemLeagueIndexName, cancellationToken).ConfigureAwait(false);
+    }
+
+    public async Task UpAsync(IMongoDatabase database, CancellationToken cancellationToken = default)
+    {
+        var col = optionsAccessor.Value.GetGemPriceCollection(database);
+        IndexKeysDefinitionBuilder<PoeNinjaApiGemPriceEnvalope> builder = new();
+        var index = builder.Text(e => e.League);
+
+        CreateIndexModel<PoeNinjaApiGemPriceEnvalope> model = new(index, new()
+        {
+            Name = GemLeagueIndexName
+        });
+        _ = await col.Indexes.CreateOneAsync(model, null, cancellationToken).ConfigureAwait(false);
+    }
+
+}
+
+[MongoMigration(PoeNinjaDatabaseSettings.Alias, 1, 2, Description = $"Add text index {GemNameIndexName}.")]
+public sealed class PoeNinjaAddNameIndexMigration(IOptions<PoeNinjaDatabaseSettings> optionsAccessor) : IMongoMigration
+{
+    public const string GemNameIndexName = "GemName";
+
+    public async Task DownAsync(IMongoDatabase database, CancellationToken cancellationToken = default)
+    {
+        var col = optionsAccessor.Value.GetGemPriceCollection(database);
+        await col.Indexes.DropOneAsync(GemNameIndexName, cancellationToken).ConfigureAwait(false);
+    }
+
+    public async Task UpAsync(IMongoDatabase database, CancellationToken cancellationToken = default)
+    {
+        var col = optionsAccessor.Value.GetGemPriceCollection(database);
+        IndexKeysDefinitionBuilder<PoeNinjaApiGemPriceEnvalope> builder = new();
+        var index = builder.Text(e => e.Price.Name);
+
+        CreateIndexModel<PoeNinjaApiGemPriceEnvalope> model = new(index, new()
+        {
+            Name = GemNameIndexName
+        });
+        _ = await col.Indexes.CreateOneAsync(model, null, cancellationToken).ConfigureAwait(false);
+    }
+}
+
+[MongoMigration(PoeNinjaDatabaseSettings.Alias, 0, 1, Description = $"Add composite index {GemIdentifierIndexName}.")]
+public sealed class PoeNinjaAddIdentifierIndexMigration(IOptions<PoeNinjaDatabaseSettings> optionsAccessor) : IMongoMigration
 {
     public const string GemIdentifierIndexName = "GemIdentifier";
 
     public async Task DownAsync(IMongoDatabase database, CancellationToken cancellationToken = default)
     {
-        var gemPriceCollection = GetGemPriceCollection(optionsAccessor, database);
+        var gemPriceCollection = optionsAccessor.Value.GetGemPriceCollection(database);
         await gemPriceCollection.Indexes.DropOneAsync(GemIdentifierIndexName, cancellationToken).ConfigureAwait(false);
     }
 
     public async Task UpAsync(IMongoDatabase database, CancellationToken cancellationToken = default)
     {
-        var gemPriceCollection = GetGemPriceCollection(optionsAccessor, database);
-        IndexKeysDefinitionBuilder<PoeNinjaApiGemPrice> builder = new();
-        var combinedIndex = builder.Combine(
-            builder.Hashed(p => p.Name),
-            builder.Ascending(p => p.GemLevel),
-            builder.Ascending(p => p.GemQuality)
+        var col = optionsAccessor.Value.GetGemPriceCollection(database);
+        IndexKeysDefinitionBuilder<PoeNinjaApiGemPriceEnvalope> builder = new();
+        var index = builder.Combine(
+            builder.Text(e => e.Price.Name),
+            builder.Ascending(e => e.League),
+            builder.Ascending(e => e.Price.GemLevel),
+            builder.Ascending(e => e.Price.GemQuality)
         );
 
-        CreateIndexModel<PoeNinjaApiGemPrice> model = new(combinedIndex, new()
+        CreateIndexModel<PoeNinjaApiGemPriceEnvalope> model = new(index, new()
         {
             Unique = true,
             Name = GemIdentifierIndexName
         });
-        _ = await gemPriceCollection.Indexes.CreateOneAsync(model, null, cancellationToken).ConfigureAwait(false);
-    }
-
-    private static IMongoCollection<PoeNinjaApiGemPrice> GetGemPriceCollection(IOptions<PoeNinjaDatabaseSettings> optionsAccessor, IMongoDatabase database)
-    {
-        return database.GetCollection<PoeNinjaApiGemPrice>(optionsAccessor.Value.GemPriceCollectionName);
+        _ = await col.Indexes.CreateOneAsync(model, null, cancellationToken).ConfigureAwait(false);
     }
 }
