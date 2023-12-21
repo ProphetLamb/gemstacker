eaed2e0f489b3f3a33d8d8c768b54fbd7de82396
ProphetLamb <prophet.lamb@gmail.com>
2023-12-21 15:57:10 +0100

    Add unique index Name

--- server/src/GemLevelProtScraper/PoeDb/Migrations.cs
+++ server/src/GemLevelProtScraper/PoeDb/Migrations.cs
@@ -11,6 +11,8 @@ public sealed record PoeDbDatabaseSettings : IOptions<PoeDbDatabaseSettings>, IM
     public required string DatabaseName { get; init; }
     public required string SkillCollectionName { get; init; }
 
+    public const string Alias = "PoeDb";
+
     PoeDbDatabaseSettings IOptions<PoeDbDatabaseSettings>.Value => this;
 
     public MongoMigrableDefinition GetMigratableDefinition()
@@ -18,7 +20,7 @@ public sealed record PoeDbDatabaseSettings : IOptions<PoeDbDatabaseSettings>, IM
         return new()
         {
             ConnectionString = ConnectionString,
-            Database = new("PoeDb", DatabaseName),
+            Database = new(Alias, DatabaseName),
             MirgrationStateCollectionName = "DATABASE_MIGRATIONS"
         };
     }
@@ -30,8 +32,34 @@ public sealed record PoeDbDatabaseSettings : IOptions<PoeDbDatabaseSettings>, IM
         return GetSkillCollection(database);
     }
 
-    private IMongoCollection<PoeDbSkillEnvalope> GetSkillCollection(IMongoDatabase database)
+    internal IMongoCollection<PoeDbSkillEnvalope> GetSkillCollection(IMongoDatabase database)
     {
         return database.GetCollection<PoeDbSkillEnvalope>(SkillCollectionName);
+    }
+}
+
+[MongoMigration(PoeDbDatabaseSettings.Alias, 0, 1, Description = $"Add unique index {SkillNameIndexName}.")]
+public sealed class PoeNinjaAddNameIndexMigration(IOptions<PoeDbDatabaseSettings> optionsAccessor) : IMongoMigration
+{
+    public const string SkillNameIndexName = "Name";
+
+    public async Task DownAsync(IMongoDatabase database, CancellationToken cancellationToken = default)
+    {
+        var col = optionsAccessor.Value.GetSkillCollection(database);
+        await col.Indexes.DropOneAsync(SkillNameIndexName, cancellationToken).ConfigureAwait(false);
+    }
+
+    public async Task UpAsync(IMongoDatabase database, CancellationToken cancellationToken = default)
+    {
+        var col = optionsAccessor.Value.GetSkillCollection(database);
+        IndexKeysDefinitionBuilder<PoeDbSkillEnvalope> builder = new();
+        var index = builder.Text(e => e.Skill.Name.Id);
+
+        CreateIndexModel<PoeDbSkillEnvalope> model = new(index, new()
+        {
+            Unique = true,
+            Name = SkillNameIndexName
+        });
+        _ = await col.Indexes.CreateOneAsync(model, null, cancellationToken).ConfigureAwait(false);
     }
 }
