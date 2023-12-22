4bb547dff00b4a94952039119f88f83b1e98e1b5
ProphetLamb <prophet.lamb@gmail.com>
2023-12-22 01:26:19 +0100

    Add powered by to footer

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -35,23 +35,30 @@
 			</AppBar>
 		</svelte:fragment>
 		<svelte:fragment slot="pageFooter">
-			<div class="p-4">
-				<p>
-					<a href="/" target="_blank" rel="noreferrer" class="anchor">Home</a>
-					&nbsp;
-					<span class="opacity-10 mx-2">|</span>
-					&nbsp;
-					<a
-						href="https://github.com/ProphetLamb/poe-gemleveling-profit-calculator/blob/master/LICENSE"
-						target="_blank"
-						rel="noreferrer"
-						class="anchor">MIT License</a
-					>
-					&nbsp;
-					<span class="opacity-10 mx-2">|</span>
-					&nbsp;
-					<a href="/imprint" target="_blank" rel="noreferrer" class="anchor">Imprint</a>
-				</p>
+			<div class="flex flex-wrap items-center align-middle p-4 text-surface-600-300-token">
+				<a href="/" target="_blank" rel="noreferrer" class="anchor">Home</a>
+				&nbsp;
+				<span class="opacity-10 mx-2">|</span>
+				&nbsp;
+				<a
+					href="https://github.com/ProphetLamb/poe-gemleveling-profit-calculator/blob/master/LICENSE"
+					target="_blank"
+					rel="noreferrer"
+					class="anchor">MIT License</a
+				>
+				&nbsp;
+				<span class="opacity-10 mx-2">|</span>
+				&nbsp;
+				<a href="/imprint" target="_blank" rel="noreferrer" class="anchor">Imprint</a>
+				&nbsp;
+				<span class="opacity-10 mx-2">|</span>
+				&nbsp;Powered by &nbsp;
+				<a href="https://poe.ninja/" class="anchor flex flex-row items-center align-middle"
+					><img src="https://poe.ninja/images/ninja-logo.png" alt="" class="w-4 h-4" /> poe.ninja</a
+				>&nbsp;&amp;&nbsp;
+				<a href="https://poedb.tw/us/" class="anchor flex flex-row items-center align-middle"
+					><img src="https://poedb.tw/favicon.ico" alt="" class="w-4 h-4" /> poedb.tw</a
+				>
 			</div>
 		</svelte:fragment>
 
