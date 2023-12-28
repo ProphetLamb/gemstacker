81b229340e54e17a81b4930dd0a35afbbadef310
ProphetLamb <prophet.lamb@gmail.com>
2023-12-28 14:06:11 +0100

    use string instead of set

--- app/src/lib/client/localSettings.ts
+++ app/src/lib/client/localSettings.ts
@@ -3,11 +3,11 @@ import { localStorageStore } from "@skeletonlabs/skeleton";
 export interface LocalSettings {
   league: string
   min_experience_delta: number
-  ignored_gems: Set<string>
+  ignored_gems: string[]
 }
 
 export const localSettings = localStorageStore<LocalSettings>('poe-gemleveling-profit-calculator-local-settings', {
   league: 'Standard',
   min_experience_delta: 340000000,
-  ignored_gems: new Set()
+  ignored_gems: []
 })
