382b152ce8b808a520cbdb662f692d953b89031d
ProphetLamb <prophet.lamb@gmail.com>
2024-01-21 13:53:39 +0100

    Remove legacy python code

--- lib/start.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-# -*- coding: utf-8 -*-
-uvicorn --factory main:app --host "${HOST}" --port "${PORT}" --workers "${WORKERS}" --log-level "${LOG_LEVEL}"
--- lib/test-serve.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# -*- coding: utf-8 -*-
-python3 -m venv .venv
-.venv/bin/pip3 install -r requirements.txt
-
-# start server
-.venv/bin/python3 -m uvicorn --factory main:app --host 127.0.0.1 --port 8000 --reload
