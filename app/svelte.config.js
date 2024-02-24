3c6daeafd771112baef51bf3bcf12b8ceac8b25a
ProphetLamb <prophet.lamb@gmail.com>
2024-02-24 17:42:49 +0100

    Migrate svelte vite plugin

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,7 @@ const config = {
 	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [ vitePreprocess()],
+	preprocess: [vitePreprocess()],
 
 	vitePlugin: {
 		inspector: true,
