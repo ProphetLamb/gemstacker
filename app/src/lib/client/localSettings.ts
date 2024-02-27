0eb9dee25aaa57717fef721fd1335d1c1306c0c4
ProphetLamb <prophet.lamb@gmail.com>
2024-02-28 00:01:36 +0100

    Handle outdated leagues in settings

--- app/src/lib/client/localSettings.ts
+++ app/src/lib/client/localSettings.ts
@@ -1,4 +1,6 @@
 import { localStorageStore } from "@skeletonlabs/skeleton";
+import { derived, type Invalidator, type Subscriber } from "svelte/store";
+import { leagues } from "./leagues";
 
 export interface LocalSettings {
   league: string
@@ -6,8 +8,25 @@ export interface LocalSettings {
   exclude_gems: string[]
 }
 
-export const localSettings = localStorageStore<LocalSettings>('poe-gemleveling-profit-calculator-local-settings', {
-  league: 'Standard',
-  min_experience_delta: 340000000,
-  exclude_gems: []
-})
+function localSettingsStore() {
+  const storage = localStorageStore<LocalSettings>('poe-gemleveling-profit-calculator-local-settings', {
+    league: '',
+    min_experience_delta: 340000000,
+    exclude_gems: []
+  })
+  const reader = derived([storage, leagues], ([$storage, $leagues]) => {
+    const league = $storage.league
+    if ($leagues.length !== 0 && !$leagues.find(l => l.id == league)) {
+      $storage.league = $leagues[0].id
+    }
+    return $storage
+  })
+  return {
+    subscribe: reader.subscribe,
+    set: storage.set,
+    update: storage.update
+  }
+}
+
+
+export const localSettings = localSettingsStore()
