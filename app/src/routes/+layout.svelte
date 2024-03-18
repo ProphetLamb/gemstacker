ac00f380022cffd839cc36bf690be047e323e2bd
ProphetLamb <prophet.lamb@gmail.com>
2024-03-18 22:46:11 +0100

    Reduce blur

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -61,8 +61,8 @@
 			background-image: url(/background.png);
 			background-repeat: no-repeat;
 			z-index: -1;
-			filter: blur(calc(100vw * 0.004)) brightness(0.95);
-			-webkit-filter: blur(calc(100vw * 0.004)) brightness(0.95);
+			filter: blur(calc(100vw * 0.003)) brightness(0.95);
+			-webkit-filter: blur(calc(100vw * 0.003)) brightness(0.95);
 			image-rendering: optimizeSpeed;
 		}
 
