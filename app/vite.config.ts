b671cea44247d368ff20c903a65b49136154f02e
ProphetLamb <prophet.lamb@gmail.com>
2025-11-11 16:41:06 +0100

    remove sentry from vite

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,14 +1,7 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "prophetlamb-38d869310",
-            project: "poe-gemleveling-profit-calculator",
-            authToken: process.env.SENTRY_AUTH_TOKEN
-        }
-    }), sveltekit(), purgeCss()]
+    plugins: [sveltekit(), purgeCss()]
 });
