e4bc85a849fafd323363ad26ea1f426fcd1c5736
ProphetLamb <prophet.lamb@gmail.com>
2025-11-15 10:29:21 +0100

    Add divine orb to knownImages

--- app/src/lib/knownImages.ts
+++ app/src/lib/knownImages.ts
@@ -4,3 +4,5 @@ export const currencyGemQuality =
   'https://web.poecdn.com/image/Art/2DItems/Currency/CurrencyGemQuality.png';
 export const currencyRerollRare =
   'https://web.poecdn.com/image/Art/2DItems/Currency/CurrencyRerollRare.png';
+export const currencyModValues =
+  'https://web.poecdn.com/image/Art/2DItems/Currency/CurrencyModValues.png';
