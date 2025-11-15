ee01e5b16be7e328a517ea03b5931baca4b7e97b
ProphetLamb <prophet.lamb@gmail.com>
2025-11-15 16:04:37 +0100

    Clean up dirty local storage situations

--- app/src/lib/localStorage.ts
+++ app/src/lib/localStorage.ts
@@ -1,8 +1,37 @@
 import { localStorageStore } from "@skeletonlabs/skeleton"
 import { isNullOrEmpty } from "./obj"
 import type { Writable } from "svelte/store"
+interface Serializer<T> {
+    parse(text: string): T;
+    stringify(object: T): string;
+}
+type StorageType = 'local' | 'session';
+interface Options<T> {
+    serializer?: Serializer<T>;
+    storage?: StorageType;
+}
+function getStorage(type: StorageType) {
+    return type === 'local' ? localStorage : sessionStorage;
+}
 
-export function localStorageStoreAllowNull<T extends object>(key: string) {
+function ensureValidStorage<T>(key: string, options?: Options<T>) {
+  const storageType = options?.storage ?? 'local'
+  const serializer = options?.serializer ?? JSON
+  const storage = getStorage(storageType)
+  const initialValue = storage.getItem(key)
+  if (initialValue === "undefined" || initialValue === "null") {
+    storage.removeItem(key)
+  } else if (initialValue !== null) {
+    try {
+      serializer.parse(initialValue)
+    } catch {
+      storage.removeItem(key)
+    }
+  }
+}
+
+export function localStorageStoreAllowNull<T extends object>(key: string, options?: Options<T>) {
+  ensureValidStorage(key, options);
   const { set, subscribe, update } = localStorageStore<T | {}>(key, {})
   return {
     set: (v) => set(!isNullOrEmpty(v) ? v as T : {}),
