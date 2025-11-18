ca5fe99889789673c8de0ac9e1ce237b94188641
ProphetLamb <prophet.lamb@gmail.com>
2025-11-18 19:30:47 +0100

    Highlight gems as corrupted if the product is corrupted

--- app/src/lib/client/GemTableIdentifier.svelte
+++ app/src/lib/client/GemTableIdentifier.svelte
@@ -15,7 +15,7 @@
 </script>
 
 <td class="pr-2">
-	<a href={gem.foreign_info_url} target="_blank" class="badge-icon h-11 w-11 {gem.min.corrupted ? 'variant-soft-error' : 'variant-soft-primary'}">
+	<a href={gem.foreign_info_url} target="_blank" class="badge-icon h-11 w-11 {gem.max.corrupted ? 'variant-soft-error' : 'variant-soft-primary'}">
 		<img src={gem.icon} alt={`${idx + 1}`} />
 	</a>
 </td>
