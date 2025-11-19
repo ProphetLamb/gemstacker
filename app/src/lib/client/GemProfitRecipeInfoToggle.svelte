1d11c97b35e4637a12e19edd47b80812cb6eb353
ProphetLamb <prophet.lamb@gmail.com>
2025-11-19 21:27:50 +0100

    Hide toggle on launch

--- app/src/lib/client/GemProfitRecipeInfoToggle.svelte
+++ app/src/lib/client/GemProfitRecipeInfoToggle.svelte
@@ -5,16 +5,13 @@
 </script>
 
 <button
-	class="z-10 card p-2 variant-soft-surface flex flex-row items-center space-x-2 {!$inspectProfit.visible &&
-	$inspectProfit.gem
-		? ''
-		: 'invisible opacity-0'} transition-all {$$props.class}"
+	class="z-[5] card p-2 variant-soft-surface hidden flex-row items-center space-x-2 {$inspectProfit.gem
+		? '!flex'
+		: ''} {!$inspectProfit.visible ? '' : 'invisible opacity-0'} transition-all {$$props.class}"
 	on:click={() => ($inspectProfit.visible = !$inspectProfit.visible)}
 >
 	<span>Recipe Info</span>
-    <button
-        type="button"
-        class="btn-icon variant-filled-success w-8 h-8"
-        ><Icon src={hi.ChevronLeft} size="18" /></button
-    >
+	<button type="button" class="btn-icon variant-filled-success w-8 h-8"
+		><Icon src={hi.ChevronLeft} size="18" /></button
+	>
 </button>
