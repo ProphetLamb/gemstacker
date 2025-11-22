0cc251a0855ea185d745c2a0b2c3b8bfea87226d
ProphetLamb <prophet.lamb@gmail.com>
2025-11-22 11:58:14 +0100

    Fix double corrupt probabilities

--- server/src/GemLevelProtScraper/Profit/Recipes/LevelDoubleCorruptAddLevelAndQualitySell.cs
+++ server/src/GemLevelProtScraper/Profit/Recipes/LevelDoubleCorruptAddLevelAndQualitySell.cs
@@ -1,4 +1,5 @@
-﻿using GemLevelProtScraper.Skills;
+﻿using System.Diagnostics.CodeAnalysis;
+using GemLevelProtScraper.Skills;
 
 namespace GemLevelProtScraper.Profit.Recipes;
 
@@ -36,6 +37,9 @@ public class LevelDoubleCorruptAddLevelAndQualitySell : IProfitRecipe
         );
     }
 
+    [SuppressMessage("Style", "IDE0048:Add parentheses for clarity")]
+    [SuppressMessage("Style", "IDE0047:Remove unnecessary parentheses")]
+    [SuppressMessage("ReSharper", "ArrangeMissingParentheses")]
     public static ProfitMargin ProfitMarginUnchecked(
         SkillProfitCalculationContext ctx,
         SkillGemPrice corruptAddLevel,
@@ -50,70 +54,66 @@ public class LevelDoubleCorruptAddLevelAndQualitySell : IProfitRecipe
         Dictionary<string, double> recipeCost = new()
         {
             [CurrencyTypeName.GemcuttersPrism] = corruptAddLevel.GemQuality - min.GemQuality, // 20 quality
-            [CurrencyTypeName.DivineOrb] = 1, // "Lapidary Lens" isn't currency we value it with 1div for now
+            [CurrencyTypeName.ChaosOrb] = 50, // "Lapidary Lens" isn't currency we value it with 50c for now
         };
 
-        // lens corrupts twice and cant choose the same outcome twice
-        // probability for a successful event is
-        // - the event -> anything else = 1/8 * 4/6
-        // - anything else -> the event = 5/8 * 1/6
-        // anything else := exclude double event lvl21/23, and we exclude rem quality, or rem level
-        // depending on whether were calculating add quality or add level
-        var pOne = (1 / 8.0 * (5 / 6.0)) + (6 / 8.0 * (1 / 6.0));
-        // probability for two events is 1/8 * 1/6
-        var pTwo = 1 / 8.0 * 1 / 6.0;
+        var addLevel23q = (1 / 8.0m) * ((1 / 3.0m) * (8 / 21.0m)) + ((1 / 4.0m) * (8 / 21.0m)) * (1 / 6.0m);
+        var addLevel20q = (1 / 8.0m) * (1 / 3.0m) + (2 / 8.0m) * (1 / 6.0m);
+        var addLevel10q = addLevel20q - addLevel23q;
+        var maxLevel23q = (3 / 4.0m) * ((1 / 3.0m) * (8 / 21.0m)) + ((1 / 4.0m) * (8 / 21.0m)) * (2 / 3.0m);
+        var anyLevel10q = maxLevel23q + addLevel23q; // add and remove level & quality have the same chances
+        var remLevelAnyQ = 1 / 8.0 + 7 / 8.0 * 1 / 6.0;
         List<ProbabilisticProfitMargin> probabilistic =
         [
             new()
             {
-                Chance = pTwo,
+                Chance =
+                    (double)addLevel23q,
                 Earnings = corruptAddLevel23Quality.ChaosValue - min.ChaosValue,
-                Label = "corrupt_add_level_add_quality",
+                Label = "double_corrupt_add_level_add_quality",
             },
             new()
             {
-                Chance = pTwo,
+                Chance = (double)addLevel10q,
                 Earnings = corruptAddLevelRemQuality.ChaosValue - min.ChaosValue,
-                Label = "corrupt_add_level_rem_quality"
+                Label = "double_corrupt_add_level_rem_quality"
             },
             new()
             {
-                Chance = pOne, Earnings = corruptAddLevel.ChaosValue - min.ChaosValue, Label = "corrupt_add_level",
+                Chance = (double)addLevel20q,
+                Earnings = corruptAddLevel.ChaosValue - min.ChaosValue,
+                Label = "double_corrupt_add_level_max_quality",
             },
             new()
             {
-                Chance = pOne,
+                Chance = (double)maxLevel23q,
                 Earnings = corruptAddQuality.ChaosValue - min.ChaosValue,
-                Label = "corrupt_add_quality"
+                Label = "double_corrupt_max_level_add_quality"
             },
-            // probability for remove quality is
-            // - the event -> anything else = 1/8 * 6/6
-            // - anything else-> the event = 7/8 * 1/6
             new()
             {
-                Chance = (1 / 8.0) + (7 / 8.0 * (1 / 6.0)),
+                Chance = (double)anyLevel10q,
                 Earnings = corruptRemQuality.ChaosValue - min.ChaosValue,
-                Label = "corrupt_rem_quality"
+                Label = "double_corrupt_corrupt_any_level_rem_quality"
             },
-            // probability for no change is
-            // - no change and vaal -> same, but no change or vaal = 5/8 * 2/6
-            // - remove level -> remainder = 1/8 * 1/6
-            // anything else := exclude add level, add quality, remove quality
             new()
             {
-                Chance = (5 / 8.0 * (2 / 6.0)) + pTwo,
+                Chance = (double)(1.0m - anyLevel10q - maxLevel23q - addLevel20q - addLevel10q - addLevel23q),
                 Earnings = corruptFailure.ChaosValue - min.ChaosValue,
                 Label = "no_change"
             },
         ];
 
         var corruptExperienceRemoveLevel = ctx.Skill.LastLevelExperience
-                                           * pOne
+                                           * remLevelAnyQ
                                            * ctx.ExperienceFactor(ctx.GemQuality(min));
         var levelExperience = ctx.Skill.SumExperience * ctx.ExperienceFactor(ctx.GemQuality(min));
         var deltaExperience = levelExperience + corruptExperienceRemoveLevel;
         var levelEarning = ctx.ProbabilisticEarnings(probabilistic) - ctx.RecipeCost(recipeCost);
 
+        // replace the divine with lapidary lens for output
+        _ = recipeCost.Remove(CurrencyTypeName.DivineOrb);
+        recipeCost["Lapidary Lens"] = 1;
         return new()
         {
             GainMargin = ctx.GainMargin(levelEarning, deltaExperience),
